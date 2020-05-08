b3e15cc0c10f08952a31a946ab1cbe98f0ccc0c7
Taylor <taylorhcarroll1@gmail.com>
2020-05-08 16:16:38 -0700

    rolled back to prev iteration, adding i to param now works

--- NatzHarmonyCapstone/Controllers/UserController.cs
+++ NatzHarmonyCapstone/Controllers/UserController.cs
@@ -245,16 +245,15 @@ namespace NatzHarmonyCapstone.Controllers
 
         private List<ApplicationUser> MatchEngine(ApplicationUser user)
         {
-            //if (user.GenderPref == false && user.CountryPref == false && user.LanguagePref == false)
+            using (SqlConnection conn = Connection)
             {
-                using (SqlConnection conn = Connection)
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    conn.Open();
-                    using (SqlCommand cmd = conn.CreateCommand())
+                    if (user.GenderPref == false && user.CountryPref == false && user.LanguagePref == false)
                     {
-                        {
-                            
-                            cmd.CommandText = @"SELECT TOP 3 u.Id, u.FirstName, u.LastName, u.Mentor, u.Gender, u.CountryId, u.[Availability], u.AvatarUrl,
+                        //refactor to include availability as part of ranking
+                        cmd.CommandText = @"SELECT TOP 3 u.Id, u.FirstName, u.LastName, u.Mentor, u.Gender, u.CountryId, u.[Availability], u.AvatarUrl,
                                             ul.LanguageId, l.[Name]
                                             FROM AspNetUsers u
                                             LEFT JOIN UserLanguage ul ON ul.UserId = u.Id
@@ -262,40 +261,74 @@ namespace NatzHarmonyCapstone.Controllers
                                             GROUP BY u.Id, u.FirstName, u.LastName, u.Mentor, u.Gender, u.CountryId, u.[Availability], ul.LanguageId, l.[Name], u.AvatarUrl
                                             HAVING u.Mentor = 1
                                             ORDER BY u.LastName DESC";
-                        }
-                        
-                        var reader = cmd.ExecuteReader();
-                        var matches = new List<ApplicationUser>();
+                    }
+                    else
+                    {
+                        cmd.CommandText = @"SELECT TOP 3 u.Id, u.FirstName, u.LastName, u.Mentor, u.Gender, u.CountryId, u.[Availability], u.AvatarUrl,
+                                        ul.LanguageId, l.[Name],
+                                        + COUNT(CASE WHEN u.[Availability] = '@Availability' THEN 1 END) ";
 
-                        while (reader.Read())
+                        cmd.Parameters.Add(new SqlParameter("@Availability", user.Availability));
+
+                        if (user.GenderPref == true)
                         {
-                            var match = new ApplicationUser()
-                            {
-                                Id = reader.GetString(reader.GetOrdinal("Id")),
-                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
-                                //AvatarUrl = reader.GetString(reader.GetOrdinal("AvatarUrl")),
-                                Gender = reader.GetString(reader.GetOrdinal("Gender")),
-                                Availability = reader.GetString(reader.GetOrdinal("Availability")),
-                                CountryId = reader.GetInt32(reader.GetOrdinal("CountryId"))
+                            cmd.CommandText += " + COUNT(CASE WHEN u.Gender = '@Gender' THEN 1 END) ";
+                            cmd.Parameters.Add(new SqlParameter("@Gender", user.Gender));
+                        }
 
-                            };
-                            //checks against db to return nullable AvatarUrl 
-                            if (!reader.IsDBNull(reader.GetOrdinal("AvatarUrl")))
+                        if (user.CountryPref == true)
+                        {
+                            cmd.CommandText += "  + COUNT(CASE WHEN u.CountryId = @Country THEN 1 END) ";
+                            cmd.Parameters.Add(new SqlParameter("@Country", user.CountryId));
+                        }
+
+                        if (user.LanguagePref == true)
+                        {
+                            for (var i = 0; i < user.Languages.Count; i++)
+                            //foreach (UserLanguage lang in user.Languages)
                             {
-                                match.AvatarUrl = reader.GetString(reader.GetOrdinal("AvatarUrl"));
+                                cmd.CommandText += " + COUNT(CASE WHEN ul.LanguageId = @Lang" + i + " THEN 1 END)  ";
+                                cmd.Parameters.Add(new SqlParameter("@Lang"+i, user.Languages[i].Language.LanguageId));
                             }
 
-                            matches.Add(match);
                         }
-                        reader.Close();
-                        return matches;
+                        cmd.CommandText += @" AS CriteriaRank
+                                            FROM AspNetUsers u
+                                            LEFT JOIN UserLanguage ul ON ul.UserId = u.Id
+                                            LEFT JOIN[Language] l ON l.LanguageId = ul.LanguageId
+                                            GROUP BY u.Id, u.FirstName, u.LastName, u.Mentor, u.Gender, u.CountryId, u.[Availability], ul.LanguageId, l.[Name], u.AvatarUrl
+                                            HAVING u.Mentor = 1
+                                            ORDER BY CriteriaRank DESC, u.LastName DESC ";
+
                     }
+                    var reader = cmd.ExecuteReader();
+                    var matches = new List<ApplicationUser>();
+
+                    while (reader.Read())
+                    {
+                        var match = new ApplicationUser()
+                        {
+                            Id = reader.GetString(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            //AvatarUrl = reader.GetString(reader.GetOrdinal("AvatarUrl")),
+                            Gender = reader.GetString(reader.GetOrdinal("Gender")),
+                            Availability = reader.GetString(reader.GetOrdinal("Availability")),
+                            CountryId = reader.GetInt32(reader.GetOrdinal("CountryId"))
+
+                        };
+                        //checks against db to return nullable AvatarUrl 
+                        if (!reader.IsDBNull(reader.GetOrdinal("AvatarUrl")))
+                        {
+                            match.AvatarUrl = reader.GetString(reader.GetOrdinal("AvatarUrl"));
+                        }
+
+                        matches.Add(match);
+                    }
+                    reader.Close();
+                    return matches;
                 }
             }
-
-
-
         }
     }
 }
